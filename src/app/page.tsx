aede66c3300b8bb90bbc6e62c1e48ca0d80cd836
Lev Giffune <levgiffune@gmail.com>
2024-11-03 21:20:14 -0500

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,42 @@
 import React from 'react';
 import {Model, Board, Move} from '../model';
 import {redraw} from '../boundary'
-import {config1, config2, config3} from '../config'
+
+const config1 = {
+  "name": "#1",
+  "words" : [ "CYAN", "YELLOW", "PURPLE", "MAUVE", "BLUE" ],
+  "theme" : "Colors",
+  "initial": [ ['E', 'L', 'W', 'Y', 'C'],
+               ['Y', 'L', 'O', 'A', 'N'],
+               ['U', 'B', 'L', 'E', 'E'],
+               ['E', 'L', 'P', 'M', 'V'],
+               ['P', 'U', 'R', 'A', 'U']
+             ]
+}
+  
+const config2 = {
+  "name": "#2",
+  "words" : [ "TAPIR", "EAGLE", "JAGUAR", "SNAKE", "WOLF"],
+  "theme" : "Animals",
+  "initial": [ ['E', 'K', 'O', 'A', 'P'],
+               ['A', 'W', 'L', 'I', 'R'],
+               ['N', 'S', 'F', 'A', 'T'],
+               ['L', 'E', 'E', 'R', 'A'],
+               ['A', 'G', 'G', 'U', 'J']
+             ]
+}
+
+const config3 = {
+  "name": "#3",
+  "words" : [ "CHERRY", "PAPAYA", "BANANA", "PEAR", "FIG" ],
+  "theme" : "Fruits",
+  "initial": [ ['H', 'C', 'N', 'A', 'N'],
+               ['Y', 'R', 'A', 'A', 'A'],
+               ['R', 'E', 'A', 'Y', 'B'],
+               ['F', 'P', 'P', 'E', 'R'],
+               ['I', 'G', 'A', 'P', 'A']
+             ]
+}
 
 
 export function select(x: number, y: number, board: Board, ctx: any){
@@ -69,26 +104,28 @@ export default function Home() {
 
   return (
     <body>
-      <h1>Score: <span className="score">{score}</span></h1>
-      <h1>Moves: <span className="moves">{moves}</span></h1>
-      <label id="buttons">
-          <button id="reset" onClick={(e) => {reset()}}>reset</button>
-          <button id="check" onClick={(e) => {
-            setWin(model.checker());
-          }}>check solution</button>
-          <span id="message" className={win == "Congratulations!" ? "green" : "red"}>{win}</span>
-      </label>
-      <label id="dropdown">
-            Choose a puzzle theme
-            <select
-              value={conf.name.substring(1)}
-              onChange={(e) => {setConf(eval("config" + e.target.value))}}
-            >
-              <option value="1">{config1.theme}</option>
-              <option value="2">{config2.theme}</option>
-              <option value="3">{config3.theme}</option>
-            </select>
-      </label>
+      <h1>Score: {score}</h1>
+      <h1>Moves: {moves}</h1>
+      <h1 className={win == "Congratulations!" ? "green" : "red"}>{win}</h1>
+      <div className="line">
+        <label id="buttons">
+            <button id="reset" onClick={(e) => {reset()}}>reset</button>
+            <button id="check" onClick={(e) => {
+              setWin(model.checker());
+            }}>check solution</button>
+        </label>
+        <label id="dropdown">
+              Choose a puzzle theme
+              <select
+                value={conf.name.substring(1)}
+                onChange={(e) => {setConf(eval("config" + e.target.value))}}
+              >
+                <option value={1}>{config1.theme}</option>
+                <option value={2}>{config2.theme}</option>
+                <option value={3}>{config3.theme}</option>
+              </select>
+        </label>
+      </div>
       <canvas id="canvas" ref={canvasRef} onClick={(e) =>{
         model.board.selected = select(e.clientX, e.clientY, model.board, canvasRef.current);
         redraw(canvasRef.current, model.board);
--- src/boundary.test.ts
+++ src/boundary.test.ts
@@ -1,9 +1,20 @@
 import { expect, test } from "vitest"
 import Home from "./app/page"
-import {config1} from './config'
 import { Model } from "./model";
 
-let m = new Model(JSON.stringify(config1));
+const config = {
+    "name": "#1",
+    "words" : [ "CYAN", "YELLOW", "PURPLE", "MAUVE", "BLUE" ],
+    "theme" : "Colors",
+    "initial": [ ['E', 'L', 'W', 'CY', ''],
+                 ['Y', 'L', 'O', 'A', 'N'],
+                 ['U', 'B', 'L', 'E', 'E'],
+                 ['E', 'L', 'P', 'M', 'V'],
+                 ['P', 'U', 'R', 'A', 'U']
+               ]
+  }
+
+let m = new Model(JSON.stringify(config));
 test("select", () => {
     
 })
