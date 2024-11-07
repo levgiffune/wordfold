5085959ef3fff155a39d7bdbb0b112b8dab2a0df
Lev Giffune <levgiffune@gmail.com>
2024-11-07 13:49:11 -0500

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    exclude: ["*.config.*"],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**',
+       '**/.{idea,git,cache,output,temp}/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
   },
 })
