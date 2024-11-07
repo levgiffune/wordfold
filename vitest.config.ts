6e8f0cee19c4688d9c25ba50da6f115339c92cf5
Lev Giffune <levgiffune@gmail.com>
2024-11-07 14:08:06 -0500

    updated testing excludes

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      exclude: ['./*.config.*', './next-env.d.ts', './src/app/layout.tsx', './.next/**'],
+    },
     environment: 'jsdom',
-    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**',
-       '**/.{idea,git,cache,output,temp}/**',
-        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
   },
 })
